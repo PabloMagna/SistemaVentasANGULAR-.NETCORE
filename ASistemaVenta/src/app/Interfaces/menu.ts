4c04a121b2c1f14f3a87f36a06a251c74e755609
PabloMagna <pablo.magnavachi@alumnos.frgp.utn.edu.ar>
2023-09-16 13:26:03 -0300

    creados servicios e interfaces necesarias para la comunicación con la API

---
+++ ASistemaVenta/src/app/Interfaces/menu.ts
@@ -0,0 +1,6 @@
+export interface Menu {
+    idMenu:number,
+    nombre:string,
+    url:string,
+    icono:string,
+}
---
+++ ASistemaVenta/src/app/Interfaces/producto.ts
@@ -0,0 +1,8 @@
+export interface Producto {
+    idProducto:number,
+    nombre:string,
+    idCategoria:number,
+    descripcionCategoria:string,
+    stock:string,
+    esActivo:number
+}
